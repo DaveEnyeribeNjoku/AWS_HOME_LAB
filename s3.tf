ac3dd60146b4410fee42ca0c147c10136de1fc53
David Njoku <DnjFinance@outlook.fr>
2025-11-22 20:32:02 +0100

    s3.tf

--- s3.tf
+++ s3.tf
@@ -1,24 +1,28 @@
-# S3 bucket sécurisé + versioning + lifecycle + CloudWatch alarm
-resource "aws_s3_bucket" "Homelab_backup" {
-  bucket = "david-njoku-Homelab-backup-2025"
-  
+# S3 bucket sécurisé + versioning + lifecycle
+resource "aws_s3_bucket" "homelab_backup" {
+  bucket = "david-njoku-homelab-backup-2025"
+
   tags = {
-    Name    = "Homelab-Backup"
-    Owner   = "David Njoku"
-    Mission = "Accelerate the world's transition"
+    Name  = "homelab-backup"
+    Owner = "David Njoku"
   }
 }
 
 resource "aws_s3_bucket_versioning" "versioning" {
-  bucket = aws_s3_bucket.Homelab_backup.id
-  versioning_configuration { status = "Enabled" }
+  bucket = aws_s3_bucket.homelab_backup.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
 
 resource "aws_s3_bucket_lifecycle_configuration" "lifecycle" {
-  bucket = aws_s3_bucket.Homelab_backup.id
+  bucket = aws_s3_bucket.homelab_backup.id
+
   rule {
     id     = "delete-after-90d"
     status = "Enabled"
-    expiration { days = 90 }
+    expiration {
+      days = 90
+    }
   }
 }
